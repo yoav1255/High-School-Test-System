535ddbe84bdfdd30a17ddfc784c6a3b1ceb14d00
AdiRaz-code <adiraz1997@gmail.com>
2023-05-18 17:13:46 +0300

    hi

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Controllers/CreateQuestionController.java
@@ -0,0 +1,23 @@
+package il.cshaifasweng.OCSFMediatorExample.client.Controllers;
+
+import il.cshaifasweng.OCSFMediatorExample.client.App;
+import il.cshaifasweng.OCSFMediatorExample.client.SimpleClient;
+import il.cshaifasweng.OCSFMediatorExample.entities.Teacher;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import org.greenrobot.eventbus.EventBus;
+
+import java.io.IOException;
+public class CreateQuestionController {
+    @FXML
+    private Label subjectLabel;
+
+    @FXML
+    private ChoiceBox subjectChoiceBox;
+
+
+
+}
